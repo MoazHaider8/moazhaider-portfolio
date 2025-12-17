e7b95eb4944de1a1f464becb38fb3c7c50014392
MoazHaider8 <moazhaider8@gmail.com>
2025-12-17 17:14:15 +0500

    Fix double header/footer issue - remove Header/Footer from LayoutWrapper since they're in layout.tsx

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import Header from "./Header";
-import Footer from "./Footer";
-
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
+  return <>{children}</>;
 }
